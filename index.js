eab67bd44eb29a5112c94f368af2a78b72a4c629
Moosadeveloper <janmoosa77@gmail.com>
2024-04-15 21:49:07 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var a =prompt("first name");
+
+
+var country = prompt("Country?","China");
+
+var yourName = prompt("Enter Your Name");
+
+
+var c = prompt("Enter Your Name?","Moosa");
+
+var b = "hi"
+var d ="moosa"
+var e =prompt(b,d)
+
+var u = prompt("Please enter your favorite color:", "Blue");
+alert("Your favorite color is: " + u);
